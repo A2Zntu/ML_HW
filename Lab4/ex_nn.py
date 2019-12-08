541c4e53d58038e6d30106cf43b4a639f5d34a70
I FAN CHIANG <35946036+A2Zntu@users.noreply.github.com>
2019-12-08 12:52:53 +0100

    add lab5 file

--- Lab4/ex_nn.py
+++ Lab4/ex_nn.py
@@ -31,7 +31,7 @@ debugMode = False
 
 # Setup the parameters you will use for this exercise
 input_layer_size = 400;     # 20x20 Input Images of Digits
-hidden_layer_size = 50;     # 25 hidden units
+hidden_layer_size = 25;     # 25 hidden units
 num_labels = 10;            # 10 labels, from 0 to 9   
                             # (note that we have mapped "0" to label 9 to follow
                             # the same structure used in the MatLab version)
